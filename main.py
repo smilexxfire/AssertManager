04b41c53b8c0e1997e2dec5bceba0bde5a5dbdc2
smilexxfire <1160177189@qq.com>
2024-04-11 01:55:53 +0800

    update

--- main.py
+++ main.py
@@ -56,8 +56,37 @@ def txt_to_db():
     input_dir = "asserts"
     format_all_to_db(input_dir)
 
+def check():
+    # 检查配置文件是否存在
+    print("检查配置文件是否存在...")
+    if not os.path.exists("default.ini"):
+        print("配置文件不存在，请编写default.ini文件")
+        sys.exit(1)
+    # 检查数据库连接是否正常
+    print("检查数据库连接...")
+    db = conn_db("asserts")
+
+
+    # 为数据库创建索引
+    try:
+        # 创建索引，并设置 background 参数为 True
+        db.create_index([("domain", pymongo.ASCENDING)])
+        print("创建索引...")
+
+    except pymongo.errors.OperationFailure as e:
+        # 检查错误消息是否为索引已存在的错误
+        if "An existing index has the same name as the requested index" in str(e):
+            print("索引已存在...")
+        else:
+            # 如果错误消息不是索引已存在的错误，则重新引发异常
+            raise e
+
+    print("检查通过 enjoy it...")
+
 if __name__ == '__main__':
+    check()
     if "--purge" in sys.argv:
         db = conn_db("asserts")
         db.delete_many({})
     txt_to_db()
+    print("资产已全部入库...")
--- readme.md
+++ readme.md
@@ -7,14 +7,15 @@
 - mongo
 
 使用步骤
-1. 在mongodb中依次运行mongo.db文件中的两条语句，以创建索引
-2. 将mongo连接信息填入default.ini.sample文件，并重命名为default.ini
-3. 安装项目依赖 ```pip install -r requirements.txt```
-4. 运行`python3 main.py`，asserts目录下的txt文件会自动被扫描并进入数据库
-5. 运行`python3 main.py --purge`，会先清空数据库，再进行入库
+1. 将mongo连接信息填入default.ini.sample文件，并重命名为default.ini
+2. 安装项目依赖 ```pip install -r requirements.txt```
+3. 运行`python3 main.py`，asserts目录下的txt文件会自动被扫描并进入数据库
+4. 运行`python3 main.py --purge`，会先清空数据库，再进行入库
 
 添加资产: asserts目录下新增txt文件即可，第一行为企业名称，之后每一行为一个域名，重要的域名在结尾标记如`baidu.com -`
 
 todo
 - [x] 支持入库前清空数据库，以同步本地更改
 - [x] 支持重要域名标记
+- [x] 运行前检查环境
+- [x] 自动添加索引
